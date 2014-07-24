24df9c3f0a88500cc4be9345237cff4cd1d4f81a
leeway1208 <liweiwang@liweis-MacBook-Pro.local>
2014-07-24 19:03:51 +0800

    UI update

--- src/com/twinly/eyebb/activity/ChildrenListActivity.java
+++ src/com/twinly/eyebb/activity/ChildrenListActivity.java
@@ -32,7 +32,12 @@ public class ChildrenListActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_children_list);
-		setTitle(getString(R.string.text_kids_list));
+		if (getIntent().getIntExtra("from", 0) == 2){
+			setTitle(getString(R.string.text_change_kids));
+		}else if (getIntent().getIntExtra("from", 0) == 1) {
+			setTitle(getString(R.string.text_kids_list));
+		}
+		
 
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		getActionBar().setDisplayShowHomeEnabled(false);
